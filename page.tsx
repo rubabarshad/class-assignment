c81e0df3a5b61b1605dc38b8c365708bd08b8840
Rubab Arshad <160515174+rubabarshad@users.noreply.github.com>
2024-10-01 21:02:19 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,12 @@
+
+import Header from "@/app/components/header";
+import Footer from "@/app/components/footer";
+export default function Home() {
+  return (
+        <div>
+          <Header/>
+          <Footer/>
+        </div>
+        
+  );
+}
